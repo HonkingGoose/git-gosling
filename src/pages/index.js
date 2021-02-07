7c4f86c86e7c9656c08cd41412091c218ebf19be
HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
2021-02-07 18:12:56 +0100

    style: use green call to action button in index.js hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -54,7 +54,7 @@ function Home() {
           <div className={styles.buttons}>
             <Link
               className={clsx(
-                "button button--outline button--secondary button--lg",
+                "button button--success button--lg",
                 styles.getStarted
               )}
               to={useBaseUrl("docs/")}
